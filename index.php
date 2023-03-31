08b256be8e6993737388f2897de006d55bee044e
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-31 14:39:12 +0000

    added code

---
+++ index.php
@@ -0,0 +1,62 @@
+<DOCTYPE html>
+<html lang="en-ca">
+  <head>
+
+    <!--Meta data-->
+    <meta charset="utf-8">
+    <meta name="description" content="=Volume of Triangular Pyramid">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="ZoiaB">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index1/apple-touch-ion.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index1/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index1/favicon-16x16.png">
+    <link rel="manifest" href="./fav_index1/site.webmanifest">
+
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="./css/style.css">
+    
+    <!--MDL theme-->
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_purple.min.css">
+
+    <!--Title-->
+    <title>Calculating the Volume of a Triangular Pyramid in PHP</title>
+  </head>
+  <body>
+    <h1>Calculating the Volume of a Triangular Pyramid in PHP with User Input</h1>
+    
+    <!--Div-->
+    <div id="div">
+      <h3>Insert you input:</h3>
+
+    <!--Image-->
+      <center>
+        <img src="./images/pyramid.png" width="300" length="300">
+      </center>
+
+    <!--Form for user input-->
+      <center>
+      <form action="./results.php" method="post" target="display-result">
+        <label for="pyramid-height">Pyramid Height (H)</label>
+        <input type="float" name="pyramid-height" placeholder="Pyramid height (cm)...">
+        <br>
+        <br>
+        <label for="base-length">Base Length (A)</label>
+        <input type="float" name="base-length" placeholder="Base lenght (cm)...">
+        <br>
+        <br>
+        <label for="base-height">Base Height (B)</label>
+        <input type="float" name="base-height" placeholder="Base height (cm)...">
+        <br>
+        <br>
+        <input type="submit" value="Calculate" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+      </form>
+    </div>
+
+    <!--Iframe to display results-->
+    <iframe id="display-result" name="display-result"></iframe>
+      </center>
+  </body>
+</html>
---
+++ results.php
@@ -0,0 +1,13 @@
+<?php
+  //Get the user input
+  $heightOfTriangularPyramid = floatval($_POST["pyramid-height"]);
+  $sideAOfTriangularPyramid = floatval($_POST["base-length"]);
+  $sideBOfTriangularPyramid = floatval($_POST["base-height"]);
+
+  //Calculate the volume
+  $volume = $sideAOfTriangularPyramid * $sideBOfTriangularPyramid *   $heightOfTriangularPyramid / 6;
+  $volumeRounded = round($volume, 2);
+?>
+
+<h3>Results:</h3>
+The volume of the triangular pyramid is <?php echo "$volumeRounded" ?> cm<sup>2</sup>.<br>
